f46d28156420f3853cfa5e49f7b4804fdcfa96d5
Huan Nguyen <Huanvip2kk@gmail.com>
2022-07-02 18:36:23 +0700

    Change

--- lib/presentation/home/ui/home_screen.dart
+++ lib/presentation/home/ui/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Demo app'),
       ),
       body: const Center(
-        child: Text('Hello baybe'),
+        child: Text('Lo cc'),
       ),
     );
   }
